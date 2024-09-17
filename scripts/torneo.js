073c0aa9794edbda2e2fcd56b59bbf5d024ce876
Sebas <83621478+Sartentooth@users.noreply.github.com>
2024-09-17 16:50:33 -0300

    Update torneo.js

--- scripts/torneo.js
+++ scripts/torneo.js
@@ -1,23 +1,17 @@
-// Cargar los puntos desde el localStorage, si existen
-const players = JSON.parse(localStorage.getItem("players")) || {
-  Alros: 0,
-  Lolo: 0,
-  Tortopulus: 0,
-  Sarten: 0,
-  Melkor: 0,
-  Marqués: 0,
-  Lvlaster: 0,
-  Nifunifaz: 0,
-};
-
 // Función para actualizar los puntos y guardarlos en localStorage
-function guardarResultado(winner, loser, isChecked) {
-  if (isChecked) {
+function guardarResultado(winner, loser, winnerCheckbox, loserCheckbox) {
+  if (winnerCheckbox.checked && !loserCheckbox.checked) {
     players[winner] += 3;
+    players[loser] += 0;
+  } else if (!winnerCheckbox.checked && loserCheckbox.checked) {
+    players[loser] += 3;
+    players[winner] += 0;
   } else {
-    players[winner] -= 3;
+    // Si ambos están marcados o desmarcados, no hacemos nada
+    return;
   }
-  localStorage.setItem("players", JSON.stringify(players)); // Guardar en localStorage
+  
+  localStorage.setItem("players", JSON.stringify(players));
   updateRanking();
 }
 
@@ -34,7 +28,7 @@ function updateRanking() {
   sortedPlayers.forEach(([player, points], index) => {
     const row = document.createElement('tr');
     row.innerHTML = `
-     <td>${index + 1}</td>
+      <td>${index + 1}</td>
       <td>${player}</td>
       <td>${points}</td>
     `;
